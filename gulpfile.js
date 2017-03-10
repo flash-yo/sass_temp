f2b17c96930f52c34a4d0d6ea5928c835c96f1fd
y.ogawa <y.ogawa@leihauoli.com>
2017-03-10 15:24:46 +0900

    サーバー立ち上げ追加

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var scssLint = require('gulp-scss-lint');
+var webserver = require('gulp-webserver');
+
 
 // Sassコンパイルタスク
 gulp.task('sass', function(){
@@ -13,18 +15,30 @@ gulp.task('sass', function(){
 });
 
 // watchタスク(**/*.scss変更時に実行するタスク)
-gulp.task('sass-watch', ['scss-lint','sass'], function(){
+gulp.task('sass-watch', ['scss-lint','sass','webserver'], function(){
 	var watcher = gulp.watch('src/sass/**/*.scss', ['scss-lint','sass']);
 	watcher.on('change', function(event) {
 		console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
 	});
 });
 
-
+//sass-lint
 gulp.task('scss-lint', function(){
 	return gulp.src('src/sass/**/*.scss')
 		.pipe(scssLint());
 });
-// gulpのデフォルト動作としてsass-watchを実行
 
+
+//web sever
+gulp.task('webserver', function () {
+	gulp.src('') // 公開したい静的ファイルを配置したディレクトリを指定する
+		.pipe(webserver({
+			host: 'localhost',
+			port: 8000,
+			browser: 'Google Chrome',
+			livereload: true
+		}));
+});
+
+// gulpのデフォルト動作としてsass-watchを実行
 gulp.task('default', ['sass-watch']);
